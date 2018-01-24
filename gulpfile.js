1e7d9cbf0eff9fbc310a1187d927a9c87514a2ac
Guan Gui <guan@southbanksoftware.com>
2018-01-25 09:29:01 +1100

    Removed `deb` in artifacts and fixed S3 region

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
  * @Date:   1970-01-01T10:00:00+10:00
  * @Email:  root@guiguan.net
  * @Last modified by:   guiguan
- * @Last modified time: 2018-01-25T05:30:10+11:00
+ * @Last modified time: 2018-01-25T09:28:04+11:00
  *
  * dbKoda - a modern, open source code editor, for MongoDB.
  * Copyright (C) 2017-2018 Southbank Software
@@ -158,7 +158,6 @@ gulp.task('addVersionSuffixToBuildArtifact', (cb) => {
   pump(
     [
       gulp.src([
-        './dbkoda/dist/*.deb',
         './dbkoda/dist/*.AppImage',
         './dbkoda/dist/*.yml',
         './dbkoda/dist/*.sha1'
